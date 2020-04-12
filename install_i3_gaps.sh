ea15a3d2c5827b35bbee9081464c74eea9d314e4
Michael Millis <mmillis1@gmail.com>
2020-04-11 17:16:46 -0700

    fix i3 gaps

--- install_i3_gaps.sh
+++ install_i3_gaps.sh
@@ -1,23 +1,18 @@
 #!/usr/bin/env bash
 
-sudo apt install libxcb-shape0-dev libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake
+sudo apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf xutils-dev libtool libxcb-shape0-dev 
 
 cd ~/Downloads/
 mkdir tmp
-cd tmp
-
-# clone the repository
+cd /tmp
 git clone https://www.github.com/Airblader/i3 i3-gaps
 cd i3-gaps
-
-# compile & install
+git checkout gaps && git pull
 autoreconf --force --install
-rm -rf build/
-mkdir -p build && cd build/
-
-# Disabling sanitizers is important for release versions!
-# The prefix and sysconfdir are, obviously, dependent on the distribution.
-../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
+rm -rf build
+mkdir build
+cd build
+../configure --prefix=/usr --sysconfdir=/etc
 make
 sudo make install
 cd ~/Downloads
